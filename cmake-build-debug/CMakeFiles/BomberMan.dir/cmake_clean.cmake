e11ed569869f41a4cc7da61a4689372adb1973b6
Jan-Hendrik Hoon <janhoon@e5r11p4.wtc.co.za>
2017-08-11 13:18:06 +0200

    clion init

---
+++ cmake-build-debug/CMakeFiles/BomberMan.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BomberMan.dir/main.cpp.o"
+  "BomberMan.pdb"
+  "BomberMan"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BomberMan.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
