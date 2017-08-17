357dd5840c6c877c242d99768c8a7c97bd962d6f
Vern01 <verdunw2@gmail.com>
2017-08-17 20:33:29 +0200

    built abstract

---
+++ back/abstracts/includes/Bot.hpp
@@ -0,0 +1,18 @@
+#ifndef PROJECT_BOT_HPP
+#define PROJECT_BOT_HPP
+
+class Bot {
+public:
+	virtual check_available_move() = 0;
+
+protected:
+	int		_dir_x;
+	int		_dir_y;
+	bool	_up;
+	bool	_down;
+	bool	_left;
+	bool	_right;
+
+};
+
+#endif //PROJECT_BOT_HPP
