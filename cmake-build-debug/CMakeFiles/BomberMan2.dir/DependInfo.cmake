4d04950e10f4d3d7ac8cca181468efdc406d6e0f
Victor Verdun VAN DER WATT <vivan-de@e5r11p4.wethinkcode.co.za>
2017-09-14 07:38:34 +0200

    Bomb is displayed on the map

--- cmake-build-debug/CMakeFiles/BomberMan2.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/BomberMan2.dir/DependInfo.cmake
@@ -4,21 +4,21 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/vern/Documents/wtc/BomberMan/Map.cpp" "/home/vern/Documents/wtc/BomberMan/cmake-build-debug/CMakeFiles/BomberMan2.dir/Map.cpp.o"
-  "/home/vern/Documents/wtc/BomberMan/abstracts/Bot.cpp" "/home/vern/Documents/wtc/BomberMan/cmake-build-debug/CMakeFiles/BomberMan2.dir/abstracts/Bot.cpp.o"
-  "/home/vern/Documents/wtc/BomberMan/abstracts/Object.cpp" "/home/vern/Documents/wtc/BomberMan/cmake-build-debug/CMakeFiles/BomberMan2.dir/abstracts/Object.cpp.o"
-  "/home/vern/Documents/wtc/BomberMan/abstracts/Object_movable.cpp" "/home/vern/Documents/wtc/BomberMan/cmake-build-debug/CMakeFiles/BomberMan2.dir/abstracts/Object_movable.cpp.o"
-  "/home/vern/Documents/wtc/BomberMan/factories/Factory.cpp" "/home/vern/Documents/wtc/BomberMan/cmake-build-debug/CMakeFiles/BomberMan2.dir/factories/Factory.cpp.o"
-  "/home/vern/Documents/wtc/BomberMan/main.cpp" "/home/vern/Documents/wtc/BomberMan/cmake-build-debug/CMakeFiles/BomberMan2.dir/main.cpp.o"
-  "/home/vern/Documents/wtc/BomberMan/objects/Bomb.cpp" "/home/vern/Documents/wtc/BomberMan/cmake-build-debug/CMakeFiles/BomberMan2.dir/objects/Bomb.cpp.o"
-  "/home/vern/Documents/wtc/BomberMan/objects/Bomberman.cpp" "/home/vern/Documents/wtc/BomberMan/cmake-build-debug/CMakeFiles/BomberMan2.dir/objects/Bomberman.cpp.o"
-  "/home/vern/Documents/wtc/BomberMan/objects/Empty.cpp" "/home/vern/Documents/wtc/BomberMan/cmake-build-debug/CMakeFiles/BomberMan2.dir/objects/Empty.cpp.o"
-  "/home/vern/Documents/wtc/BomberMan/objects/Soft_wall.cpp" "/home/vern/Documents/wtc/BomberMan/cmake-build-debug/CMakeFiles/BomberMan2.dir/objects/Soft_wall.cpp.o"
-  "/home/vern/Documents/wtc/BomberMan/objects/Squid.cpp" "/home/vern/Documents/wtc/BomberMan/cmake-build-debug/CMakeFiles/BomberMan2.dir/objects/Squid.cpp.o"
-  "/home/vern/Documents/wtc/BomberMan/observers/Object_subject.cpp" "/home/vern/Documents/wtc/BomberMan/cmake-build-debug/CMakeFiles/BomberMan2.dir/observers/Object_subject.cpp.o"
-  "/home/vern/Documents/wtc/BomberMan/observers/Subject.cpp" "/home/vern/Documents/wtc/BomberMan/cmake-build-debug/CMakeFiles/BomberMan2.dir/observers/Subject.cpp.o"
+  "/goinfre/vivan-de/Documents/BomberMan/Map.cpp" "/goinfre/vivan-de/Documents/BomberMan/cmake-build-debug/CMakeFiles/BomberMan2.dir/Map.cpp.o"
+  "/goinfre/vivan-de/Documents/BomberMan/abstracts/Bot.cpp" "/goinfre/vivan-de/Documents/BomberMan/cmake-build-debug/CMakeFiles/BomberMan2.dir/abstracts/Bot.cpp.o"
+  "/goinfre/vivan-de/Documents/BomberMan/abstracts/Object.cpp" "/goinfre/vivan-de/Documents/BomberMan/cmake-build-debug/CMakeFiles/BomberMan2.dir/abstracts/Object.cpp.o"
+  "/goinfre/vivan-de/Documents/BomberMan/abstracts/Object_movable.cpp" "/goinfre/vivan-de/Documents/BomberMan/cmake-build-debug/CMakeFiles/BomberMan2.dir/abstracts/Object_movable.cpp.o"
+  "/goinfre/vivan-de/Documents/BomberMan/factories/Factory.cpp" "/goinfre/vivan-de/Documents/BomberMan/cmake-build-debug/CMakeFiles/BomberMan2.dir/factories/Factory.cpp.o"
+  "/goinfre/vivan-de/Documents/BomberMan/main.cpp" "/goinfre/vivan-de/Documents/BomberMan/cmake-build-debug/CMakeFiles/BomberMan2.dir/main.cpp.o"
+  "/goinfre/vivan-de/Documents/BomberMan/objects/Bomb.cpp" "/goinfre/vivan-de/Documents/BomberMan/cmake-build-debug/CMakeFiles/BomberMan2.dir/objects/Bomb.cpp.o"
+  "/goinfre/vivan-de/Documents/BomberMan/objects/Bomberman.cpp" "/goinfre/vivan-de/Documents/BomberMan/cmake-build-debug/CMakeFiles/BomberMan2.dir/objects/Bomberman.cpp.o"
+  "/goinfre/vivan-de/Documents/BomberMan/objects/Empty.cpp" "/goinfre/vivan-de/Documents/BomberMan/cmake-build-debug/CMakeFiles/BomberMan2.dir/objects/Empty.cpp.o"
+  "/goinfre/vivan-de/Documents/BomberMan/objects/Soft_wall.cpp" "/goinfre/vivan-de/Documents/BomberMan/cmake-build-debug/CMakeFiles/BomberMan2.dir/objects/Soft_wall.cpp.o"
+  "/goinfre/vivan-de/Documents/BomberMan/objects/Squid.cpp" "/goinfre/vivan-de/Documents/BomberMan/cmake-build-debug/CMakeFiles/BomberMan2.dir/objects/Squid.cpp.o"
+  "/goinfre/vivan-de/Documents/BomberMan/observers/Object_subject.cpp" "/goinfre/vivan-de/Documents/BomberMan/cmake-build-debug/CMakeFiles/BomberMan2.dir/observers/Object_subject.cpp.o"
+  "/goinfre/vivan-de/Documents/BomberMan/observers/Subject.cpp" "/goinfre/vivan-de/Documents/BomberMan/cmake-build-debug/CMakeFiles/BomberMan2.dir/observers/Subject.cpp.o"
   )
-set(CMAKE_CXX_COMPILER_ID "GNU")
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
